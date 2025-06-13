b6334c02d1d28a521483654e4168935f0846d111
CaioLima10 <caiohenrys50@gmail.com>
2025-06-13 15:38:20 -0300

    themes global css

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import "./styles/themes.css";
+import "./styles/global.css";
+import { Heading } from "./components/heading";
+
 export function App() {
-  return <></>;
+  return (
+    <div>
+      <Heading />
+      <div></div>
+    </div>
+  );
 }
