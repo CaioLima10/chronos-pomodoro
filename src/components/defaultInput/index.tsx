75f498eb80823970f593e47775a22f0fc61b9448
CaioLima10 <caiohenrys50@gmail.com>
2025-07-02 16:27:28 -0300

    finalizing form

--- src/components/defaultInput/index.tsx
+++ src/components/defaultInput/index.tsx
@@ -1,13 +1,20 @@
+import styles from "./styles.module.css";
+
 type DefaultInputProps = {
   id: string;
   textLabel: string;
 } & React.ComponentProps<"input">;
 
-export function DefaultInput({ id, textLabel, type }: DefaultInputProps) {
+export function DefaultInput({
+  id,
+  textLabel,
+  type,
+  ...rest
+}: DefaultInputProps) {
   return (
     <>
       <label htmlFor={id}>{textLabel}</label>
-      <input id={id} type={type} />
+      <input className={styles.input} id={id} type={type} {...rest} />
     </>
   );
 }
